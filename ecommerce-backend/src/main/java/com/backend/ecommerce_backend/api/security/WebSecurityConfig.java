839302264f790de7026e67b723ad4e2fe4fd46da
Ghaith-Bassam-Zaza <ghaith.zaza@outlook.com>
2024-09-26 17:19:08 +0300

    secured endpoints

--- ecommerce-backend/src/main/java/com/backend/ecommerce_backend/api/security/WebSecurityConfig.java
+++ ecommerce-backend/src/main/java/com/backend/ecommerce_backend/api/security/WebSecurityConfig.java
@@ -5,14 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class WebSecurityConfig {
+    JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll());
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers("/product", "/auth/login", "/auth/register").permitAll()
+                .anyRequest().authenticated());
         return http.build();
     }
 }
