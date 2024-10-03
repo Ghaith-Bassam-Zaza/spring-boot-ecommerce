a956db22966356de80cc399f9b3ee89f721c0f09
Ghaith-Bassam-Zaza <ghaith.zaza@outlook.com>
2024-10-03 07:48:00 +0300

    solved primary key conflict error when running all tests together

--- ecommerce-backend/src/test/java/com/backend/ecommerce_backend/api/controller/auth/AuthenticationControllerTest.java
+++ ecommerce-backend/src/test/java/com/backend/ecommerce_backend/api/controller/auth/AuthenticationControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class AuthenticationControllerTest {
--- ecommerce-backend/src/test/java/com/backend/ecommerce_backend/service/UserServiceTest.java
+++ ecommerce-backend/src/test/java/com/backend/ecommerce_backend/service/UserServiceTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -25,6 +26,7 @@ import java.util.List;
 
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UserServiceTest {
 
 
