7b0ea3b1db731c6354a50f73fe4b6bc0cc8ca746
Ghaith-Bassam-Zaza <ghaith.zaza@outlook.com>
2024-10-13 23:06:37 +0300

    Added websocket configration.

---
+++ ecommerce-backend/src/main/java/com/backend/ecommerce_backend/api/security/WebSocketConfig.java
@@ -0,0 +1,64 @@
+package com.backend.ecommerce_backend.api.security;
+
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authorization.AuthorizationEventPublisher;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.authorization.SpringAuthorizationEventPublisher;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+
+
+@Configuration
+@EnableWebSocket
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final ApplicationContext context;
+
+    public WebSocketConfig(ApplicationContext context) {
+        this.context = context;
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").setAllowedOrigins("**").withSockJS();//set allowed origin is only to bypass cors
+
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+
+    }
+
+    private AuthorizationManager<Message<?>> makeMassageAuthorizationManager(){
+        MessageMatcherDelegatingAuthorizationManager.Builder messages = new MessageMatcherDelegatingAuthorizationManager.Builder();
+        // authenticate messages to path and permit messages to any other and deny all messages of type message
+        messages.simpDestMatchers("/topic/user/**").authenticated()
+                .simpTypeMatchers(SimpMessageType.MESSAGE).denyAll()
+                .anyMessage().permitAll();
+        return messages.build();
+
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        AuthorizationManager<Message<?>> authorizationManager = makeMassageAuthorizationManager();
+        AuthorizationChannelInterceptor authInterceptor = new AuthorizationChannelInterceptor(authorizationManager);
+
+        AuthorizationEventPublisher publisher = new SpringAuthorizationEventPublisher(context);
+        authInterceptor.setAuthorizationEventPublisher(publisher);
+
+        registration.interceptors(authInterceptor);
+    }
+}
